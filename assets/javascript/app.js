8446675660f42bc3720ee857c13b916f9b1845b7
Golden Dragon <arajchwald@gmail.com>
2019-03-09 16:13:35 -0600

    yeet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,186 +1,161 @@
 
-
-var queryURL = 'https://opentdb.com/api.php?amount=15&difficulty=medium&type=multiple';
-var questions = [];
-var questionBank = [];
-var questionAnswers = [];
-var correctAnswer;
-var wins = 0;
-var loss = 0;
-var count = 0;
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    // console.log(response);
-
-    for (let i = 0; i < response.results.length; i++) {
-
-        var mystring = response.results[i].question;
-        mystring = mystring.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, "é");
-
-        console.log(mystring);
-
-        // var string = mystring.replaceAll("^\"|\"$", "");
+$(document).ready(function () {
 
 
+    var queryURL = 'https://opentdb.com/api.php?amount=3&difficulty=medium&type=multiple';
 
-        questions.push(response.results[i]);
-        // questionBank.push(response.results[i].question);
-        questionBank.push(mystring);
-        questionAnswers.push(response.results[i].correct_answer);
+    var questions = [];
+    var questionBank = [];
+    var questionAnswers = [];
+    var correctAnswer;
+    var wins = 0;
+    var loss = 0;
+    var count = 0;
 
 
+    $('.gameStart').on('click', function () {
+
+        console.log($('.diff').val())
+        var nmbQ = $('.nQ').val()
+        var diff = $('.diff').val()
+        var genre = $('.genre').val()
+
+        $('.gameStart').css({ 'display': 'none' })
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            for (let i = 0; i < response.results.length; i++) {
+
+                var mystring = response.results[i].question;
+                mystring = mystring.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, "é");
+
+                console.log(mystring);
+                questions.push(response.results[i]);
+                questionBank.push(mystring);
+                questionAnswers.push(response.results[i].correct_answer);
+            }
+            setQuestion()
+        })
+    })
+
+    function setQuestion() {
+
+        if (count < questions.length) {
+            $('.answer').empty();
+            correctAnswer = questions[count].correct_answer;
+            $(".ques").empty();
+            $(".quest").empty();
+            $(".ques").text(questionBank[count]);
+            var a = questions[count].incorrect_answers[0].replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, "é");
+            var b = questions[count].incorrect_answers[1].replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, "é");
+            var c = questions[count].incorrect_answers[2].replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, "é");
+            var d = questions[count].correct_answer.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, "é");
+            var newQuestion = [a, b, c, d];
+            var k = shuffle(newQuestion);
+            //adds questions into question divs 
+            for (let i = 0; i < newQuestion.length; i++) {
+                $('.q' + i).text(k[i]);
+            }
+        }
+        else {
+            endScreen();
+        }
+        count++;
     }
 
-    console.log(questionAnswers);
-    console.log(questionBank);
-    setQuestion()
-})
+    $(document).on('click', '.quest', function () {
 
+        var k = $(this).text();
+        if (k == correctAnswer) {
+            gotRight();
+        }
+        else {
+            gotWrong();
+        }
+    })
 
-function setQuestion() {
-
-    $(".ques").empty();
-    $(".quest").empty();
-    $(".ques").text(questionBank[count]);
-    var a = questions[count].incorrect_answers[0].replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, "é");
-    var b = questions[count].incorrect_answers[1].replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, "é");
-    var c = questions[count].incorrect_answers[2].replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, "é");
-    var d = questions[count].correct_answer.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, "é");
-
-    // var newQuestion = [questions[count].incorrect_answers[0], questions[count].incorrect_answers[1], questions[count].incorrect_answers[2], questions[count].correct_answer];
-    var newQuestion = [a, b, c, d];
-
-    var k = shuffle(newQuestion);
-
-    //adds questions into question divs 
-    for (let i = 0; i < newQuestion.length; i++) {
-
-        $('.q' + i).text(k[i]);
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
     }
-    correctAnswer = questions[count].correct_answer;
 
-}
-
-$(document).on("click", ".q0", function () {
-
-    var k = $(".q0").text();
-    if (k == correctAnswer) {
-
+    var myVar;
+    function gotRight() {
         wins++;
-        $(".answer").text("CORRECT ANSWER!! ");
+        $(".answer").text("Correct Answer! Nice job ;)");
         $(".winLog").text('Wins: ' + wins);
-        middleCard();
-
+        myVar = setTimeout(setQuestion, 3000)
     }
-    else {
+
+    function gotWrong() {
+        loss++;
         $(".answer").text("WRONG!  Correct Answer: " + correctAnswer);
-        loss++;
         $(".lossLog").text('Losses: ' + loss);
+        // setTimeout(setQuestion(), 5000);
+        myVar = setTimeout(setQuestion, 3000)
     }
-});
-$(document).on('click', ".q1", function () {
-    var k = $('.q1').text();
-    if (k == correctAnswer) {
 
-        $('.answer').text("CORRECT ANSWER!! ")
-        wins++;
-        $('.winLog').text('Wins: ' + wins);
-        middleCard();
-    }
-    else {
-        $('.answer').text("WRONG!  Correct Answer: " + correctAnswer)
-        loss++;
-        $(".lossLog").text('Losses: ' + loss);
-    }
-})
-$(document).on('click', ".q2", function () {
-    var k = $('.q2').text();
-    if (k == correctAnswer) {
-
-        $('.answer').text("CORRECT ANSWER!! ")
-        wins++;
-        $('.winLog').text('Wins: ' + wins);
-        middleCard();
-    }
-    else {
-        $('.answer').text("WRONG!  Correct Answer: " + correctAnswer)
-        loss++;
-        $(".lossLog").text('Losses: ' + loss);
-    }
-})
-$(document).on('click', ".q3", function () {
-    var k = $('.q3').text();
-
-    if (k == correctAnswer) {
-
-        $('.answer').text("CORRECT ANSWER!! ")
-        wins++;
-        $('.winLog').text('Wins: ' + wins);
-        middleCard();
-    }
-    else {
-        $('.answer').text("WRONG!  Correct Answer: " + correctAnswer)
-        loss++;
-        $(".lossLog").text('Losses: ' + loss);
-    }
-})
-
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
-}
-
-
-
-var number = 0;
-var intervalId;
-var bool = false;
-function decrement() {
-    number--;
-    $("#show-number").html("<h2>" + number + "</h2>");
-    if (number === 0 && bool == true) {
-
-        number = 15;
+    function endScreen() {
         $('.answer').empty();
-        setQuestion();
-        bool = false;
-        $('.answer').empty();
+        $('.winLog').empty();
+        $('.lossLog').empty();
+        $('.ques').empty();
+        $('.questionContainer').empty();
+        $('.gameOver').html('SCORE: <br>' + wins + ' CORRECT <br>' + loss + ' INCORRECT')
+
     }
-    else if (number === 0 && bool == false) {
-        // setQuestion();
-        middleCard();
-    }
-}
 
-function run() {
+})
+// var number = 0;
+// var intervalId;
+// var bool = false;
+// function decrement() {
+//     number--;
+//     $("#show-number").html("<h2>" + number + "</h2>");
+//     if (number === 0 && bool == true) {
 
-    clearInterval(intervalId);
-    number = 15;
-    intervalId = setInterval(decrement, 1000);
-}
+//         number = 15;
+//         $('.answer').empty();
+//         setQuestion();
+//         bool = false;
+//         $('.answer').empty();
+//     }
+//     else if (number === 0 && bool == false) {
+//         // setQuestion();
+//         middleCard();
+//     }
+// }
 
-function middleCard() {
+// function run() {
 
-    $('.answer').text('Correct Answer!' + questions[count].correct_answer);
-    count++;
-    clearInterval(intervalId);
-    number = 5;
-    intervalId = setInterval(decrement, 1000);
-    bool = true;
+//     clearInterval(intervalId);
+//     number = 15;
+//     intervalId = setInterval(decrement, 1000);
+// }
 
-}
+// function middleCard() {
 
-run();
+//     $('.answer').text('Correct Answer!' + questions[count].correct_answer);
+//     count++;
+//     clearInterval(intervalId);
+//     number = 5;
+//     intervalId = setInterval(decrement, 1000);
+//     bool = true;
+
+// }
+
+// run();
